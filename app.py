b516ee6016b43cb83f62ae8aec0bede63bb64357
david-morales <dmoralesdf@gmail.com>
2023-05-16 22:45:13 +0200

    Different approach for certificates

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
+    # Concatenate the certificate and key into a single PEM file at runtime
+    pem_file_path = "/.tls.pem"
+    with open(pem_file_path, 'w') as pem_file:
+        with open('/certificates/tls.crt', 'r') as crt_file:
+            pem_file.write(crt_file.read())
+        with open('/certificates/tls.key', 'r') as key_file:
+            pem_file.write(key_file.read())
+
     # Use the concatenated certificate and key file in the MongoClient connection
     client = MongoClient('mongodb', 27017, 
                             tls=True, 
-                            tlsCertificateKeyFile='./tls.pem', 
-                            tlsCAFile='./ca.pem')
+                            tlsCertificateKeyFile=pem_file_path, 
+                            tlsCAFile='/certificates/ca.crt')
     db = client['demo']
     collection = db['names']
     name = collection.find_one()['name']
