7fe40b2c8447b638158373414adb19fd672f3979
david-morales <dmoralesdf@gmail.com>
2023-05-18 18:18:24 +0200

    add tls

--- app.py
+++ app.py
@@ -5,7 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    client = MongoClient('mongodb', 27017)
+    # Concatenate the certificate and key into a single PEM file at runtime
+    pem_file_path = "/tmp/tls.pem"
+    with open(pem_file_path, 'w') as pem_file:
+        with open('/certificates/tls.crt', 'r') as crt_file:
+            pem_file.write(crt_file.read())
+        with open('/certificates/tls.key', 'r') as key_file:
+            pem_file.write(key_file.read())
+
+    # Use the concatenated certificate and key file in the MongoClient connection
+    client = MongoClient('mongodb', 27017, 
+                            tls=True, 
+                            tlsCertificateKeyFile=pem_file_path, 
+                            tlsCAFile='/certificates/ca.crt'
+                            )
+
     db = client['demo']
     collection = db['names']
     name = collection.find_one()['name']
